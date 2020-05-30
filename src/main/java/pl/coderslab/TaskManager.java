9ad428b66cb5f91106fb3be976b294af8d5c46f5
Michal Wieczorek <mwalberto11@gmail.com>
2020-05-30 13:23:32 +0200

    1 Commit

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -4,10 +4,6 @@ public class TaskManager {
 
     public static void main(String[] args) {
         System.out.println("HI");
-        System.out.println("elo");
-        System.out.println("elo");
-        System.out.println("elo");
-        System.out.println("elo");
-        System.out.println("elo");
+
     }
 }
